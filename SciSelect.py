e30364dba2c1a275962979154afad21adad6f59a
trustIndex-golden <47246953+trustIndex-golden@users.noreply.github.com>
2019-02-01 21:24:28 +0100

    Add files via upload
    Code for fetching a various number of random articles from the previous extracted list from web of science.

---
+++ SciSelect.py
@@ -0,0 +1,28 @@
+import random
+filepath = "data\jap_2000-2001-plain.txt"
+
+with open(filepath) as f:
+    articles = f.read().strip().split("\n")
+
+articles_list = []
+
+author = ""
+title = ""
+year = ""
+doi = ""
+
+for article in articles:
+    if "AU" in article:
+        author = article.split("#")[-1]
+    if "TI" in article:
+        title = article.split("#")[-1]
+    if "PY" in article:
+        year = article.split("#")[-1]
+    if "DI" in article:
+        doi = article.split("#")[-1]
+    if article == "ER#":
+        articles_list.append("{}, {}, {}, doi:{}".format(author, title, year, doi))
+
+print("Oh hello sir, how many articles do you like to get?")
+amount = input()
+print(random.sample(articles_list, k = int(amount)))
