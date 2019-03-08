a6c9eb068e0a7af2f490e5e64831c2de1b1fdf5a
trustIndex-golden <tobias@tobiasfotografiert.de>
2019-03-08 11:56:06 +0100

    Update SciSelect.py

--- SciSelect.py
+++ SciSelect.py
@@ -2,17 +2,15 @@ import random
 import sys
 from fpdf import FPDF
 import datetime
-from colorama import init
-init(strip=not sys.stdout.isatty()) # strip colors if stdout is redirected
-from termcolor import cprint
 from pyfiglet import figlet_format
 
-# Some pretty stuff
-cprint(figlet_format('TrustIndex', font='big'),
-       'yellow', 'on_red')
+# The title
+print(figlet_format('TrustIndex', font='big'))
 print("___________________________________________________________________________________")
 
-# function for finding duplicates. Otherwise it would return the same index for duplicates, like: 2,2,2,2, for "PT J" (don't know why)
+# function for finding duplicates. Otherwise it would return the same first index for
+# all duplicates, like: 2,2,2,2, for "PT J"
+
 
 def list_duplicates_of(seq,item):
     start_at = -1
@@ -27,7 +25,10 @@ def list_duplicates_of(seq,item):
             start_at = loc
     return locs
 
-# Opening the designated file of joy:
+
+# Opening the designated file; currently, it needs to be a txt output file from web of science.
+# A sample of this output is pictured in the readme file.
+
 filepath = "data\JAP.txt"
 with open(filepath, encoding="UTF8") as f:
     articles = f.read().strip().split("\n")
@@ -36,15 +37,15 @@ with open(filepath, encoding="UTF8") as f:
 print("\n")
 print(datetime.datetime.now())
 
-while True:
 # Scanning for the name of the journal and available years:
+while True:
     journal_name = [i for i in articles if i.startswith('SO ')]
     journal_name_clean = journal_name[0].replace("SO ", "")
     journal_year = [i for i in articles if i.startswith('PY ')]
     journal_year_dup = list(set(journal_year))
     journal_year_clean = ', '.join(journal_year_dup)
 
-    # The majestic empty list:
+    # The empty list:
     articles_list = []
 
     # Defining the range of one article from start to finish:
@@ -55,11 +56,12 @@ while True:
         i = i + 1
         articles_list.append(articles[i:j])
 
-    # The dialogue
+    # The dialogue with the user
     timeprint = str(datetime.datetime.now()).replace(" ", "-").replace(".", "-").replace(":", "-")
-
-    dialogue_raw = ("Welcome sir, you are about to get a random draw out of " + str(len(articles_list)) + " articles of the \""
+    # Getting the number of articles and all scanned years.
+    dialogue_raw = ("Welcome, you are about to get a random draw out of " + str(len(articles_list)) + " articles of the \""
                     + str(journal_name_clean) + "\" from the year(s) " + str(journal_year_clean) + ". You are welcome.")
+    # Cleaning the dialogue
     dialogue1 = dialogue_raw.replace("PY ", "")
 
     print("\n")
@@ -67,16 +69,12 @@ while True:
     print("\n")
     print("Now, how many random articles do you like to get on this sunny day?")
     amount = input()
-    if( amount.isdigit()):
-        # Gotta love the random stuff:
+    # Check if entered number is a number and lower or equal to the total amount of articles
+    if amount.isdigit() and int(amount) <= int(len(articles_list)):
+        # Drawing random articles from the population file:
         random_articles = random.sample(articles_list, k = int(amount))
 
-        # Merge and replace + PDF stuff:
-        pdflist = []
-        pdf = FPDF()
-        pdf.add_page()
-        pdf.set_font("Arial", size=9)
-
+        # Creating an output file with timestamp as title (in txt format)
         f = open("output/" + timeprint + ".txt","w+")
         for i in random_articles:
             i = str(i)
@@ -85,11 +83,12 @@ while True:
             i = ''.join(i)
             # print(i, sep = "\n")
             f.write(str(i).replace(",    ", " ") + '\n')
-            pdflist.append(i)
+
 
         f.close()
-        print("You will find the chosen articles under:\n>>> output/" + str(timeprint) + ".txt <<<")
+        print("You will find the chosen articles here:\n>>> output/" + str(timeprint) + ".txt <<<")
 
+        # Setting up a new round
         while True:
             answer = input("What about another run? (y/n): ")
             if answer in ("y", "n"):
