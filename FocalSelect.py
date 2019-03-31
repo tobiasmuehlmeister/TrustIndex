f9df6ec4d270127258e726dc47331bb144719c7f
trustIndex-golden <tobias@tobiasfotografiert.de>
2019-03-31 17:53:50 +0200

    Create FocalSelect.py

---
+++ FocalSelect.py
@@ -0,0 +1,15 @@
+import pandas as pd
+
+# Assign spreadsheet filename to `file`
+file = 'data\coded_articles.xlsx'
+
+# Load spreadsheet
+xl = pd.ExcelFile(file)
+
+# Load a sheet into a DataFrame by name: df1
+df1 = xl.parse('Blad2')
+df2 = df1[['Article', 'Study', 'focal_test']].dropna(subset=['focal_test'])
+
+rng_draw = df2.sample(frac = 1.0).groupby(['Article', 'Study']).head(1)
+
+print(rng_draw)
