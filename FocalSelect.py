2a16de9a88751e23d8e72750ef1dff18a1295c75
trustIndex-golden <tobias@tobiasfotografiert.de>
2019-04-01 17:02:08 +0200

    Update FocalSelect.py

--- FocalSelect.py
+++ FocalSelect.py
@@ -10,18 +10,18 @@ xl = pd.ExcelFile(file)
 df1 = xl.parse('Blad1')
 
 # filters the loaded sheet for the columns Article, Study, and focal_test, deletes missing values in focal_test
-df2 = df1[['Article', 'Study', 'focal_test']].dropna(subset=['focal_test'])
+df2 = df1[['Article', 'Study', 'Assigned_focal_test', 'focal_test']].dropna(subset=['Assigned_focal_test', 'focal_test'])
 
 # "groupby" creates hierarchical groups. First for the "Article" column, then for the "Study" column
 # First it groups all rows from article 10, then it groups all rows from study 1, 2 or 3
 # after that, the code draws a sample of 1 from each grouped study
-rng_draw = df2.sample(frac = 1.0).groupby(['Article', 'Study']).head(1)
+rng_draw = df2.sample(frac = 1.0).groupby(['Article', 'Assigned_focal_test']).head(1)
 
 # output to csv
 rng_draw[['focal_test']].to_csv('data\\focal_output.csv', sep='\t', encoding='utf-8', index=False)
 
 # this prints the draw in total (with article and study number)
-# print(rng_draw)
+print(rng_draw)
 
 # this prints just the focal_test
 # print(rng_draw[['focal_test']])
